f479d4819061e5d31f0d5bfb4e9ea61ba2b91161
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2023-06-25 23:30:44 +0100

    Added styles

--- BookingSystem.Api/Program.cs
+++ BookingSystem.Api/Program.cs
@@ -34,7 +34,7 @@ services
 var app = builder.Build();
 
 app.MapControllers();
-
+app.UseStaticFiles();
 app.MapGet("/", async (s) =>
 {
     var applicationSettings = s.RequestServices.GetRequiredService<ApplicationSettings>();
@@ -73,25 +73,28 @@ app.MapGet("/", async (s) =>
     isDebug = true;
 #endif
     var response = s.Response;
+    await response.WriteAsync("<!DOCTYPE html><html lang=\"en-GB\"><meta charset=\"utf-8\" /><meta name=\"viewport\" content=\"width=device-width\" /><head><title>Booking System API - Server Status</title><link rel=\"stylesheet\" href=\"/normalise.css\" /><link rel=\"stylesheet\" href=\"/index.css\" /><link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\r\n<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\r\n<link href=\"https://fonts.googleapis.com/css2?family=Antic+Slab&display=swap\" rel=\"stylesheet\"></head><body><div class=\"content\">");
     await response
-        .WriteAsync($"Environment Name: {applicationSettings.EnvironmentName} {isDebug.IIf("(Development)", string.Empty)}");
+        .WriteAsync($"<h2>Server status</h2><p><span class=\"title\">Environment Name:</span>{applicationSettings.EnvironmentName} {isDebug.IIf("(Development)</p>", string.Empty)}");
     await response
-            .WriteAsync($"\r\n{isDebug.IIf("Connection string", "Database name")}: {isDebug.IIf(connection.ConnectionString, connection.Database)}");
+            .WriteAsync($"<p><span class=\"title\">{isDebug.IIf("Connection string", "Database name")}:</span>{isDebug.IIf(connection.ConnectionString, connection.Database)}</p>");
     await response
-        .WriteAsync($"\r\nConnection status: {(await connectionCheck)
+        .WriteAsync($"<p><span class=\"title\">Connection status:</span>{(await connectionCheck)
             .IIf($"Successful {hasConnectionBeenCheckedNow.IIf(string.Empty, "(Cached)")}", 
-                $"Failed: {lastException?.Message}")}");
+                $"Failed: {lastException?.Message}")}</p>");
     
     if (applicationSettings.LastSuccessfulDatabaseCheckTimestamp.HasValue)
     {
         await response
-            .WriteAsync($"\r\nLast connection check: {dateNow.Subtract(
-                applicationSettings.LastSuccessfulDatabaseCheckTimestamp.Value)}");
+            .WriteAsync($"<p><span class=\"title\">Last connection check:</span> {dateNow.Subtract(
+                applicationSettings.LastSuccessfulDatabaseCheckTimestamp.Value)}</p>");
     }
 
     await response
-        .WriteAsync($"\r\nUptime: {dateNow.Subtract( 
-            applicationSettings.StartedRunningTimestamp)}");
+        .WriteAsync($"<p><span class=\"title\">Uptime:</span>{dateNow.Subtract( 
+            applicationSettings.StartedRunningTimestamp)}</p>");
+
+    await response.WriteAsync("</div></body></html>");
 });
 app.UseSwagger();
 await app.RunAsync();
